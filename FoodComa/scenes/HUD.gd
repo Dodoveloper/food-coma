b25a357e9c6d3b2688b6a0dd6b27b879fce24712
salvob <tholalak@yahoo.it>
2020-03-14 17:05:41 +0100

    better parametric stuff

--- FoodComa/scenes/HUD.gd
+++ FoodComa/scenes/HUD.gd
@@ -1,12 +1,11 @@
 extends Control
 
 
-export var lives: int = 3 
 export var life_scene : PackedScene
 onready var label = $Label
 const OFFSET = 30
 
-func _ready():
+func initialize(lives):
 	for i in lives:
 		var life = life_scene.instance()
 		add_child(life)
@@ -14,6 +13,5 @@ func _ready():
 	
 
 func update_lives(_lives):
-	self.lives -= _lives
 	remove_child(get_child(get_child_count()-1))
 	
