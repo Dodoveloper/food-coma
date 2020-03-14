0347fa2f6f25216b0496f17327afeaa4f8a07f3a
Emin981 <58749001+Emin981@users.noreply.github.com>
2020-03-14 13:48:44 +0100

    bullet pattern

--- FoodComa/entities/spawner/spawnerBullet.gd
+++ FoodComa/entities/spawner/spawnerBullet.gd
@@ -2,23 +2,30 @@ extends Node2D
 
 export (PackedScene) var Bullet
 
-var deltaTime : float
-var spawnerTimer : float = 1.0
-var quantitySpawner : float = 4.0
-var boss
+var deltaTime : float = 0.0
+var spawnerTimer : float = 1000
 var velocityBullet : int = 200
+var delta : float
+var pattern : float
+var counter = 0
+
+func _ready():
+	randomize()
 
 func _process(d):
-	spawn(d)
-
-func spawn(d):
 	deltaTime += d
-	if deltaTime > spawnerTimer:
-		for i in range(quantitySpawner):
-			var bullet = Bullet.instance()
-			bullet.scale = Vector2(4, 4)
-			bullet.global_position = get_parent().global_position
-			bullet.setVelocity(velocityBullet)
-			bullet.setDirection(sin((deltaTime/spawnerTimer) + (0.5*PI*i)))
-			get_parent().get_parent().add_child(bullet)
-		deltaTime = 0.0
+	counter += 1
+	if counter > spawnerTimer*d:
+		counter = 0
+		pattern = 0.5 + (3-0.5) * deltaTime
+		spawn()
+
+func spawn():
+#	if deltaTime > spawnerTimer:
+	var bullet = Bullet.instance()
+	bullet.scale = Vector2(4, 4)
+	bullet.global_position = get_parent().global_position
+	bullet.setVelocity(velocityBullet)
+	bullet.setDirection(pattern)
+	get_parent().get_parent().add_child(bullet)
+#		deltaTime = 0.0
