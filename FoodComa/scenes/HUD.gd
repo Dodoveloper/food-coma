02fafda4082bd1b53ef594cf6b87704829b3a15c
salvob <tholalak@yahoo.it>
2020-03-14 16:55:52 +0100

    add ui and parametric life bar

--- FoodComa/scenes/HUD.gd
+++ FoodComa/scenes/HUD.gd
@@ -1,17 +1,19 @@
 extends Control
 
 
-export var lives: int = 3 setget set_lives
-
+export var lives: int = 3 
+export var life_scene : PackedScene
 onready var label = $Label
-
-func set_lives(value):
-	lives = value
-	label.text = str(lives)
+const OFFSET = 30
 
 func _ready():
-	label.text = str(lives)
+	for i in lives:
+		var life = life_scene.instance()
+		add_child(life)
+		life.position.x = OFFSET*i
+	
 
 func update_lives(_lives):
 	self.lives -= _lives
+	remove_child(get_child(get_child_count()-1))
 	
