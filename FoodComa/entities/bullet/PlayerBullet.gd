b4eb08508e494dc58c3b64ae15c956608ddcb1f7
ThereIs0Spoon <thereis0spoon@gmail.com>
2020-03-15 14:38:30 +0100

    Fixed death of the player bullet so that now the explosion animation is visible.

--- FoodComa/entities/bullet/PlayerBullet.gd
+++ FoodComa/entities/bullet/PlayerBullet.gd
@@ -17,8 +17,11 @@ func _physics_process(delta):
 func explode():
 	$Sprite.play("exploding")
 	speed = 0
+	velocity = Vector2(0.0, 0.0)
+	$Sprite.rotation_degrees = 0
+	self.rotation_degrees = 0
 	# TODO: Please implement timer to allow for the exploding animation to play before freeing the object!
-	assert(false)
+	yield(get_tree().create_timer(0.4), "timeout")
 	queue_free()
 
 func _on_VisibilityNotifier2D_screen_exited():
@@ -28,4 +31,4 @@ func _on_VisibilityNotifier2D_screen_exited():
 func _on_PlayerBullet_body_entered(body):
 	if body.is_in_group("boss"):
 		body.hurt(1)
-		queue_free()
+		explode()
