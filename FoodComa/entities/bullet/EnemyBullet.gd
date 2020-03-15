1069749589754f1bb8003e0948e6fc5753c517c8
ThereIs0Spoon <thereis0spoon@gmail.com>
2020-03-15 08:03:23 +0100

    Patched enemy bullet to use both enemy bullet assets and properly select between them while rotating the sprite accordingly

--- FoodComa/entities/bullet/EnemyBullet.gd
+++ FoodComa/entities/bullet/EnemyBullet.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+const HEXTANT_SIZE : float = 0.39269908169872415480783042290994
 var velocity : float
 var direction : float
 var hitpoints : int = 1
@@ -15,8 +16,46 @@ func setPosition(Pos : Vector2):
 
 func _physics_process(delta):
 	var movement : Vector2
+	var hextant : int
+	var rotdeg : float
+	var diagonal : bool
+	
+	# Obtain a value 0..15 determining which hextant the bullet flies into
+	hextant = (floor(direction / HEXTANT_SIZE) as int) % 16
+	assert((hextant >= 0) and (hextant <= 15))
+	match hextant:
+		0, 15: # East
+			rotdeg = 90
+			diagonal = false
+		1,  2: # South-east
+			rotdeg = 0
+			diagonal = true
+		3,  4: # South
+			rotdeg = 0
+			diagonal = false
+		5,  6: # South-west
+			rotdeg = -90
+			diagonal = true
+		7,  8: # West
+			rotdeg = -90
+			diagonal = false
+		9, 10: # North-west
+			rotdeg = -180
+			diagonal = true
+		11, 12: # North
+			rotdeg = -180
+			diagonal = false
+		13, 14: # North-east
+			rotdeg = -270
+			diagonal = true
+	
 	movement = polar2cartesian(velocity, direction)
 	position += movement * delta
+	if diagonal:
+		$Sprite.play("diagonal")
+	else:
+		$Sprite.play("vertical")
+	$Sprite.rotation_degrees = rotdeg
 
 func destroy():
 	set_deferred("monitoring", false)
