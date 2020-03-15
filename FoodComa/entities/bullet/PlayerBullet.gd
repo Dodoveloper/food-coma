9c3ae11803553115c2a8fd30e588ef9e623b2c1a
ThereIs0Spoon <thereis0spoon@gmail.com>
2020-03-15 08:23:10 +0100

    Added animation of the antiacid pill Added explosion animation of the antiacid pill Fixed antiacid pill being too tiny TODO: A timer is to be invoked when the pill hits its target, so that the explosion animation can play. This is not yet implemented, and an assert() has been put in there as a placeholder in order to prevent this situation from being released as-is to the public.

--- FoodComa/entities/bullet/PlayerBullet.gd
+++ FoodComa/entities/bullet/PlayerBullet.gd
@@ -9,11 +9,16 @@ func start(_position, _direction, _angle):
 	global_position = _position
 	rotation = _direction.angle() + _angle
 	velocity = Vector2(1, 0).rotated(rotation) * speed
+	$Sprite.play("running")
 
 func _physics_process(delta):
 	position += velocity * delta
 
 func explode():
+	$Sprite.play("exploding")
+	speed = 0
+	# TODO: Please implement timer to allow for the exploding animation to play before freeing the object!
+	assert(false)
 	queue_free()
 
 func _on_VisibilityNotifier2D_screen_exited():
