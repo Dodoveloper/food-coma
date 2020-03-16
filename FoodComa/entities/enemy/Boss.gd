58bf607f3e515a7735d8b62ba82e288aa5220ab6
salvob <tholalak@yahoo.it>
2020-03-16 12:48:27 +0100

    Game over and won screen

--- FoodComa/entities/enemy/Boss.gd
+++ FoodComa/entities/enemy/Boss.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 signal got_hit
+signal dead
 
 export var enemy_name: String
 export var life_points: int = 100
@@ -16,6 +17,7 @@ func initboss(root:Node, player:Node):
 func hurt(quantity):
 	if not life > 0:
 		$animator.play("death")
+		emit_signal("dead")
 	else:
 		life -= quantity
 		emit_signal("got_hit", quantity)
@@ -26,5 +28,5 @@ func _physics_process(delta):
 
 func _on_animator_animation_finished():
 	if $animator.animation == "death":
-		queue_free()
+		emit_signal("dead")
 		# cambia scena
