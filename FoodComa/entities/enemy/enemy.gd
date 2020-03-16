dccb121b9379ddb1f40385b169e330f2d8d414b5
Dodoveloper <davide.cor94@gmail.com>
2020-03-16 01:48:47 +0100

    added second boss, refactored worlds

--- FoodComa/entities/enemy/enemy.gd
+++ FoodComa/entities/enemy/enemy.gd
@@ -1,13 +1,20 @@
 extends KinematicBody2D
 
+signal got_hit
+
 export var enemy_name: String
 export var life_points: int = 100
 
 onready var life = life_points
 
-signal got_hit
-
 func hurt(quantity):
-	life -= quantity
-	emit_signal("got_hit", quantity)
-	
+	if not life > 0:
+		$animator.play("death")
+	else:
+		life -= quantity
+		emit_signal("got_hit", quantity)
+
+func _on_animator_animation_finished():
+	if $animator.animation == "death":
+		queue_free()
+		# cambia scena
--- FoodComa/scenes/World.gd
+++ FoodComa/scenes/World.gd
@@ -8,13 +8,12 @@ onready var boss = $Boss
 func _ready():
 	player_hud.initialize(player.lives)
 	boss_hud.initialize(boss.enemy_name, boss.life)
-	
+
 func _on_Player_hurt(hitpoints):
 	player_hud.update_lives(hitpoints)
 
 func _on_Player_fattened(lives):
 	player_hud.initialize(lives)
-
 
 func _on_Boss_got_hit(quantity):
 	boss_hud.hurt(quantity)
