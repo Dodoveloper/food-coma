1df9b0d62343a75c2511c64c5ea575b51719e368
ThereIs0Spoon <thereis0spoon@gmail.com>
2020-03-16 20:39:30 +0100

    Added meatball sauce

---
+++ FoodComa/entities/bullet/MeatballSauce.gd
@@ -0,0 +1,29 @@
+extends Area2D
+class_name MeatballSauce
+
+var velocity : float
+var direction : float
+var hitpoints : int = 1
+
+func _physics_process(delta):
+	var movement : Vector2
+	movement = polar2cartesian(velocity, direction)
+	position += movement * delta
+
+func setVelocity(Velocity : float):
+	velocity = Velocity
+
+func setDirection(Direction : float):
+	direction = Direction
+	while direction < 0.0:
+		direction += 2*PI
+
+func setPosition(Pos : Vector2):
+	position = Pos
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+func destroy():
+	# WIP
+	queue_free()
