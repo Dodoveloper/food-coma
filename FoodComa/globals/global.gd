bdf077fa7ee470bf9765564bce642f6e6a65975f
salvob <tholalak@yahoo.it>
2020-03-16 21:29:07 +0100

    thanks and stuff

--- FoodComa/globals/global.gd
+++ FoodComa/globals/global.gd
@@ -5,11 +5,15 @@ const MAX_COURSE = 2
 
 var menu = ["res://scenes/World1.tscn", "res://scenes/World2.tscn"]
 var meetup = "res://scenes/Dialogues.tscn"
+var thanks = "res://scenes/Thanks.tscn"
+
 func next_course():
 	course += 1
 	if course >= MAX_COURSE:
 		print("THANKS FOR PLAYING")
+		get_tree().change_scene(thanks)
 		return
+		
 	
 	get_tree().change_scene(meetup)
 
