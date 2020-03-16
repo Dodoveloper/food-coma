18d6920b10d3280d39774c407d6a4344333721a6
Emin981 <58749001+Emin981@users.noreply.github.com>
2020-03-16 22:48:31 +0100

    credits works

--- FoodComa/scenes/MENU.gd
+++ FoodComa/scenes/MENU.gd
@@ -3,9 +3,11 @@ extends Control
 
 
 
-func _on_Quit_pressed():
-	get_tree().quit()
 
 
 func _on_Play_pressed():
 	global.start()
+
+
+func _on_Credits_pressed():
+	get_tree().change_scene("res://scenes/Thanks.tscn")
